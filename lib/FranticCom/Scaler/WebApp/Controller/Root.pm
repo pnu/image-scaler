3f9c803f30243c54842af5e343476b89c70f53fd
Panu Ervamaa <panu.ervamaa@frantic.com>
2014-01-23 13:25:29 +0200

    serve js from url /<version>

--- lib/FranticCom/Scaler/WebApp/Controller/Root.pm
+++ lib/FranticCom/Scaler/WebApp/Controller/Root.pm
@@ -3,6 +3,7 @@ use Moose;
 use namespace::autoclean;
 use Try::Tiny;
 use FranticCom::Scaler;
+use IO::File;
 
 BEGIN { extends 'Catalyst::Controller' }
 
@@ -16,20 +17,45 @@ sub get_config : Private {
         image_host => $self->scaler->image_host,
         max_retries => $self->scaler->max_retries,
         retry_delay => $self->scaler->retry_delay,
-        trigger_url => $c->uri_for_action('/index')
+        trigger_url => $c->uri_for_action('/index_trigger')
     };
 }
 
-sub index : Path Args(0) Method('POST') {
+sub index_trigger : Path Args(0) Method('POST') {
     my ( $self, $c ) = @_;
     $c->model('Worker')->scaler($c->req->params);
     $c->res->status(204);
 }
 
+sub index_get_js : Path Args(1) Method('GET') {
+    my ( $self, $c, $version ) = @_;
+    $c->detach('unsupported_version') unless ( $version && $version eq '1' );
+    my $full_path = $c->path_to( 'root','assets','js','scaler.js' );
+    my $fh = IO::File->new( $full_path, 'r' );
+    if ( defined $fh ) {
+        binmode $fh;
+        $c->res->body( $fh );
+        $c->res->header('Content-Type' => 'application/javascript');
+    } else {
+        $c->detach('not_found');
+    }
+}
+
 sub default : Path {
     my ( $self, $c ) = @_;
+    $c->detach('not_found');
+}
+
+sub unsupported_version : Private {
+    my ( $self, $c ) = @_;
     $c->res->status(404);
-    $c->res->body('');
+    $c->res->body('unsupported version');
+}
+
+sub not_found : Private {
+    my ( $self, $c ) = @_;
+    $c->res->status(404);
+    $c->res->body('not found');
 }
 
 sub end : ActionClass('RenderView') {
