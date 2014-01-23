08b4800b47a4e5781c4dfcc5bc9075fd7b107582
Panu Ervamaa <panu.ervamaa@frantic.com>
2014-01-23 17:21:15 +0200

    allow image_host override

--- lib/FranticCom/Scaler/WebApp/Controller/Demo.pm
+++ lib/FranticCom/Scaler/WebApp/Controller/Demo.pm
@@ -9,6 +9,7 @@ BEGIN { extends 'Catalyst::Controller' }
 sub index : Path Args(0) Method('GET') {
     my ( $self, $c ) = @_;
     $c->stash->{scaler} = $c->forward('/get_config',['demo']);
+    $c->stash->{scaler}->{image_host} = $c->req->param('image_host') if $c->req->param('image_host');
 }
 
 __PACKAGE__->meta->make_immutable;
